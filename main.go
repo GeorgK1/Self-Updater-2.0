32442a28d4cf83799002b9f68471cc2b57187bae
lollipas <draakonlohe123@gmail.com>
2021-12-30 23:59:54 +0200

    Undockerised the application, modified the code

--- main.go
+++ main.go
@@ -1,79 +1,89 @@
 package main
 
 import (
-	
 	"fmt"
 	"net/http"
 	"os"
 	"os/exec"
 	"strings"
+
 	"github.com/go-git/go-git/v5"
 	git_http "github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/spf13/viper"
 )
 
 func check_error(err error) {
+
 	if err != nil {
 		panic(err)
 	}
+
 }
 
-func server(url string) {
-
+func server() {
+	viper.SetConfigFile(".env")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", r.URL.Path)	
+		fmt.Fprintf(w, "Hello, %q", r.URL.Path)
 		
-		auth:=git_http.BasicAuth{
-			Username: os.Getenv("GIT_USERNAME"),
-			Password: os.Getenv("GIT_PASSWORD"),
+		
+		err := viper.ReadInConfig()
+
+		check_error(err)
+
+		auth := git_http.BasicAuth{
+			Username: viper.GetString("GIT_USERNAME"),
+			Password: viper.GetString("GIT_PASSWORD"),
 		}
-		
+		url := viper.GetString("GIT_URL")
+
 		parsedUrl := strings.Split(url, "/")
-		path := fmt.Sprintf("../%s", parsedUrl[4])
+		path := fmt.Sprintf("./%s", parsedUrl[4])
+
 		parsedPath := strings.Split(path, ".git")[0]
-		fmt.Println(auth)
 
 		// Clone the given repository to the given directory
 
-		repo, err := git.PlainClone(parsedPath, false , &git.CloneOptions{ 
-			URL: strings.TrimSpace(url),
+		repo, err := git.PlainClone(parsedPath, false, &git.CloneOptions{
+			URL:      strings.TrimSpace(url),
 			Progress: os.Stdout,
-			Auth: &auth,
+			Auth:     &auth,
 		})
+
 		check_error(err)
-		
-		if(r.Method == "GET") {
-			fmt.Println("POST")		
-			
+
+		if r.Method == "GET" {
+			fmt.Println("POST")
+
 			wt, err := repo.Worktree()
-			
+
 			check_error(err)
-			
+
 			wt.Pull(&git.PullOptions{
-				RemoteName: os.Getenv("GIT_REMOTE"),
-				Progress: os.Stdout,
+				RemoteName: viper.GetString("GIT_REMOTE"),
+				Progress:   os.Stdout,
 			})
-		
+
 			fmt.Println("Pulled")
 
 			fmt.Println("Restarting Docker containers")
 
-			cmd := exec.Command("docker-compose", "restart")
+			cmd := exec.Command("docker-compose", "up")
 			cmd.Dir = parsedPath
+		
 			stdout, err := cmd.Output()
 
+			fmt.Println(string(stdout))
 			check_error(err)
 
-			fmt.Println(string(stdout))
 		}
 	})
-
-	
 
 	http.ListenAndServe(":8080", nil)
 }
 
 func main() {
 	fmt.Println("Starting on localhost:8080")
+	
 
-	server(os.Getenv("GIT_URL"))
+	server()
 }
